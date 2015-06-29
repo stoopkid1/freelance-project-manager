f97c73b3b180576b0b86fc8d2ad91ad9b1f1a885
Michael Loring <mdloring@gmail.com>
2015-06-29 00:25:54 -0500

    update folder structure and implement sass base

--- developer/footer.php
+++ developer/footer.php
@@ -1,25 +1,24 @@
-<footer>
-         <div class="container">
-         
-            <div class="copy text-center">
-               Copyright &copy; 2014 <a href='http://www.loringenterprises.net' target='_blank'>Loring Enterprises</a>
-               &#8212;
-               Developed by <a href='http://www.mdloring.com/' target='_blank'>Michael Loring</a>
-               &#8212;
-               Built on <a href='http://www.getbootstrap.com/' target='_blank'>Bootstrap</a>
-            </div>
-            
-         </div>
-      </footer>
-
-    <!-- jQuery & jQuery UI -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-    <!-- Bootstrap, Custom UI, Freelance Project Manager -->
-    <script src="../bootstrap/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="vendors/tinymce/js/tinymce/tinymce.min.js"></script>
-    <script src="vendors/tags/js/bootstrap-tags.min.js"></script>
-    <script src="js/custom.js"></script>
-    <script src="js/fpm.js"></script>
-  </body>
+<footer>
+         <div class="container">
+         
+            <div class="copy text-center">
+               Copyright &copy; 2014 <a href='http://www.loringenterprises.net' target='_blank'>Loring Enterprises</a>
+               &#8212;
+               Developed by <a href='http://www.mdloring.com/' target='_blank'>Michael Loring</a>
+               &#8212;
+               Built on <a href='http://www.getbootstrap.com/' target='_blank'>Bootstrap</a>
+            </div>
+            
+         </div>
+      </footer>
+    <!-- jQuery & jQuery UI -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+    <!-- Bootstrap, Custom UI, Freelance Project Manager -->
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="vendors/tinymce/js/tinymce/tinymce.min.js"></script>
+    <script src="vendors/tags/js/bootstrap-tags.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/fpm.js"></script>
+  </body>
 </html>
